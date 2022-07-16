aaa061b8e516916f106a8b3f7953504e669b256f
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2022-07-17 00:29:14 +0600

    boilerplate setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/globals.scss';
+import 'bootstrap/scss/bootstrap.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
